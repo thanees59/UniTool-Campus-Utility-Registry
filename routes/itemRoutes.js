45b30e7aa0ca5f5c6781bc9f891f2eb473648ffd
C Thanees Andrews <thaneesrock001@gmail.com>
2026-05-14 20:55:21 +0530

    created item route configuration

---
+++ routes/itemRoutes.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+import {
+    getItems,
+    createItem
+} from "../controllers/itemController.js";
+
+const router = express.Router();
+
+router.get("/", getItems);
+router.post("/", createItem);
+
+export default router;
